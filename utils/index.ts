2b455a709e199d546f78b4161035437d06670883
shubham mani tripathi <42939900+mani9k9@users.noreply.github.com>
2023-06-23 00:07:08 +0530

    Update index.ts

--- utils/index.ts
+++ utils/index.ts
@@ -35,7 +35,7 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
     const url = new URL("https://cdn.imagin.studio/getimage");
     const { make, model, year } = car;
   
-    url.searchParams.append('customer', inshubhamtripathicompany || '');
+    url.searchParams.append('customer', 'inshubhamtripathicompany' || '');
     url.searchParams.append('make', make);
     url.searchParams.append('modelFamily', model.split(" ")[0]);
     url.searchParams.append('zoomType', 'fullscreen');
